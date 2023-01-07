f9c9b083b1f4c94aa28c2cb646817dfb934068d4
Jan Peterhänsel <Jan Peterhnsel>
2023-01-07 13:08:12 +0100

    fdfksdf

--- config/polybar/block/local.sh
+++ config/polybar/block/local.sh
@@ -13,10 +13,10 @@ if [[ $(uname -n) == "PC-Main" ]];then
    export SECOND_LEFT="menu end-left-i3 i3 end-right-i3 end-right"
    export SECOND_CENTER="music-top"
    export SECOND_RIGHT="end-left net-top"
-elif [[ $(uname -n) == "jpeterhaensel-ThinkPad-L580" ]];then 
+elif [[ $(uname -n) == "adconova-pc" ]];then 
    export MAIN_LEFT="menu end-left-i3 i3 end-right-i3 keymap end-right-i3 end-right mode"
    export MAIN_CENTER="title"
-   export MAIN_RIGHT="end-left cpu sep-1 memory sep-1 battery sep-1 disk_root sep-1 date"
+   export MAIN_RIGHT="end-left cpu sep-1 memory sep-1 battery sep-1 disk_home sep-1 disk_root sep-1 date"
    export SECOND_LEFT="end-left-i3 i3 end-right-i3 end-right"
    export SECOND_CENTER="music-top"
    export SECOND_RIGHT="end-left net-top"
