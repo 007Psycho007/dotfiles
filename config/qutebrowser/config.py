a177be6fe656b07e58702b6a50e4151812bcdeeb
Psycho <lp.forever007@yahoo.de>
2023-02-17 08:58:56 +0100

    Added Qutebrowser

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,4 @@
+config.load_autoconfig(True)
+c.url.searchengines = {'DEFAULT': 'https://search.home.janpeterhaensel.de/?q={}'}
+c.url.start_pages = ["https://search.home.janpeterhaensel.de/search"]
+config.unbind('d',mode='normal')
