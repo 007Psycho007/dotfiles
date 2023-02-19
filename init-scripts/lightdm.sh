7212697b4b44fa161761d0eaf5bdc0d55ea708de
Psycho <lp.forever007@yahoo.de>
2023-02-19 17:59:55 +0100

    jkdfjdfj

--- init-scripts/lightdm.sh
+++ init-scripts/lightdm.sh
@@ -1,3 +1,6 @@
-cp -r ~/.init/evo /usr/share/lightdm-webkit/themes/lightdm-evo
+
+git clone https://github.com/AlphaNecron/lightdm-evo.git /.init/evo
+cp -r /home/psycho/.init/evo /usr/share/lightdm-webkit/themes/lightdm-evo
 sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = lightdm-evo #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
 sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
+rm -rf /.init
