bf16a64c29f42d18b527b9eda8354cfc8958d6d1
Psycho <lp.forever007@yahoo.de>
2023-02-23 16:30:54 +0100

    fdjmff

--- config/qtile/keys.py
+++ config/qtile/keys.py
@@ -51,7 +51,7 @@ keys = [
 
     Key([mod], "p", lazy.spawn(os.path.expanduser("~/.config/scripts/display-selector.sh"))),
     Key([mod], "g", lazy.spawn(os.path.expanduser("~/.config/scripts/dmenu/menu.sh")), desc="Script Menu"),
-    Key([mod], "x", lazy.spawn(os.path.expanduser("~/.config/scripts/lock/lock")), desc="Screen Lock"),
+    Key([mod], "x", lazy.spawn("betterlockscreen -l"), desc="Screen Lock"),
     Key([mod], "b", lazy.spawn(os.path.expanduser("~/.config/scripts/bwmenu/bwmenu")), desc="Screen Lock"),
     Key([mod], "m", lazy.widget["keyboardlayout"].next_keyboard(), desc="Next keyboard layout."),
     # Playerctl
