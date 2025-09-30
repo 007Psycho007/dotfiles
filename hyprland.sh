fa06b16406da6d68d9206d9afcea190836557e80
Jan <jan.peterhaensel@adconova.com>
2025-09-30 21:45:32 +0200

    Restructure

--- hyprland.sh
+++ hyprland.sh
@@ -1,8 +1,5 @@
 sudo pacman -Syu
-sudo pacman -S --needed base-devel git cmake meson ninja \
-  wayland wayland-protocols wlroots xorg-xwayland \
-  polkit elogind \
-  seatd hyprland waybar wofi kitty dunst wl-clipboard swaybg meson cmake go
+sudo pacman -Sy  base-devel git cmake meson ninja wayland wayland-protocols wlroots xorg-xwayland elogind seatd hyprland waybar wofi kitty dunst wl-clipboard swaybg meson cmake go
 
 sudo systemctl enable --now seatd.service
 
