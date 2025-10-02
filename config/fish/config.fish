45d5d833b1406a8792b1316b2f4b44a5b0e44ebf
Jan <admin@linklair.dev>
2025-10-02 08:07:05 +0200

    SSH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,8 +10,6 @@ set -x TERM xterm-kitty
 
 # --- Aliases ---
 alias ssh="kitty +kitten ssh"
-alias ns="nix-shell --extra-experimental-features flakes"
-alias nx="nix --extra-experimental-features nix-command --extra-experimental-features flakes"
 
 # --- Prompt ---
 starship init fish | source
@@ -19,9 +17,6 @@ enable_transience
 
 # --- Keybindings ---
 fish_vi_key_bindings
-
-# --- Any-nix-shell integration ---
-any-nix-shell fish | source
 
 # --- Greeting ---
 set fish_greeting
